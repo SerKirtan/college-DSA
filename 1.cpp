c2213ca7f33d6c730eb9cab8acbebb8361532182
Kirtan Patel <82870418+SerKirtan@users.noreply.github.com>
2022-11-21 12:49:08 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,13 +1,12 @@
-#include<iostream>
-using namespace std;
+#include<stdio.h>
 void insert(int *a , int n)
 {
 	int pos, num , i;
-	cout << "Enter which index postion do you want to insert an number : ";
-	cin >> pos;
+	printf("Enter which index postion do you want to insert an number : ");
+	scanf("%d", &pos);
 	pos--;
-	cout << "Enter the number : ";
-	cin >> num;
+	printf("Enter the number : ");
+	scanf("%d", &num);
 	for(i = (n-2) ; i != pos ; i--)
 		a[i+1]=a[i];
 	a[i+1]=num;
@@ -16,8 +15,8 @@ void insert(int *a , int n)
 void deletion(int *a , int n)
 {
 	int pos;
-	cout << "Enter which postion do you want to delete the number : ";
-	cin >> pos;
+	printf("Enter which postion do you want to delete the number : ");
+	scanf("%d", &pos);
 	for(int i = pos ; i < n ;  i++)
 		a[i]=a[i+1];
 }
@@ -25,38 +24,38 @@ void deletion(int *a , int n)
 void search(int *a , int n)
 {
 	int num;
-	cout << "Enter which number do you want to search : ";
-	cin >> num;
+	printf("Enter which number do you want to search : ");
+	scanf("%d", &num);
 	for(int i = 0 ; i < n ; i++)
 	{
 		if (a[i] == num)
-			cout << "Number found at index "<< i << endl;
+			printf("Number found at index %d" , i);
 	}
 }
 
 void display(int *a , int n)
 {
 	for (int i=0 ; i < n ; i++)
-		cout << i << "]" << " "<< a[i]<<endl;
+		printf("[%d] %d ", i , a[i]);
 }
 
 void input(int *a , int n)
 {
-	cout << "Enter array elements : ";
+	printf("Enter array elements : ");
 	for(int i = 0 ; i < n ; i++)
-		cin >> a[i];
+		scanf("%d" ,&a[i]);
 }
 
 int main()
 {
 	int a[50] , n , check ;
-	cout << "Enter the sixe of array [1-20] : ";
-	cin >> n;
+	printf("Enter the sixe of array [1-20] : ");
+	scanf("%d" , &n);
 	input (a,n);
 	
 	do{
-		cout << "1]Insertion\n2]Deletion\n3]Search element\n4]Display array\n5]End program\nEnter which operation you want to do : ";
-		cin >> check ;
+		printf("\n\n1]Insertion\n2]Deletion\n3]Search element\n4]Display array\n5]End program\nEnter which operation you want to do : ");
+		scanf("%d" ,&check);
 		switch(check)
 		{
 		case 1:
@@ -76,7 +75,7 @@ int main()
 		case 5:
 			break;	
 		default:
-			cout << "INVALID INPUT!! -> Enter 1-5\n";
+			printf("INVALID INPUT!! -> Enter 1-5\n");
 		}	
 	}while(check != 5);
 }
