7425b87fc39ab5dea52727a0cdd6660a70930e9f
Kirtan Patel <82870418+SerKirtan@users.noreply.github.com>
2022-12-25 13:54:42 +0530

    Create 3_LevelOrderTrees.c

---
+++ Extra/3_LevelOrderTrees.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Queue{
+    struct Node* data;
+    struct Queue* next;
+}*front , *rear;
+
+void push(struct Node* data)
+{
+    struct Queue* add = malloc(sizeof(struct Queue));
+    struct Queue* ptr = rear;
+    add->data = data;
+    add->next = NULL;
+    if( rear ==  NULL && front == NULL)
+        rear = front = add;
+    else{
+        rear = add;
+        ptr->next = rear;
+        rear->next = NULL;
+    }
+}
+
+struct Node* pop()
+{
+    if(front == NULL)
+        printf("\nQueue is empty!");
+    else{
+        struct Queue* temp = front;
+        front = front->next;
+        return temp->data;
+    }
+}
+
+struct Node* buildTree(int nodes[])//{ 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+{
+    static int idx = -1;
+    idx++;
+    if(nodes[idx] == -1)
+        return NULL;
+    
+    struct Node* newNode = malloc(sizeof(struct Node));
+    newNode->data = nodes[idx];
+    newNode->left = buildTree(nodes);
+    newNode->right = buildTree(nodes);
+    return newNode;
+}
+
+void levelOrder(struct Node* root)
+{
+    front = rear = NULL;
+    push(root);
+    push(NULL);
+    while (front != NULL)
+    {
+        struct Node* currNode = pop();
+        if(currNode == NULL)
+        {
+            printf("\n");
+            if(front == NULL)
+                break;
+            else
+                push(NULL);
+        }
+        else
+        {
+            printf("%d ", currNode->data);
+            if(currNode->left != NULL)
+                push(currNode->left);
+            if(currNode->right != NULL)
+                push(currNode->right);
+        }
+    }
+}
+
+int main()
+{
+    int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+    struct Node* root = buildTree(nodes);
+    levelOrder(root);
+}
