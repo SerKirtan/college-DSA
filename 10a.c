17ae101581a98f684f8cd8d66ff56cdc9f9503f3
Kirtan Patel <82870418+SerKirtan@users.noreply.github.com>
2023-01-03 09:48:15 +0530

    Update 10a.c

--- 10a.c
+++ 10a.c
@@ -17,7 +17,7 @@ int main()
     
     //SORTING
     for(int i = 0 ; i < size-1 ; i++)
-        for(int j = 0 ; j < size ; j++)
+        for(int j = 0 ; j < size-1-i ; j++)
             if(arr[j] > arr[j+1])
             {
                 int temp = arr[j];
